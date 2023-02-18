3fa33d1f753259f021ff3bc90d441fba9d0dbbd5
dan_kucherenko <daniil.ukraine@gmail.com>
2023-02-19 00:14:34 +0200

    Modified for calculating expressions with variables

--- polish-notation/calculator/calculate-rpn.go
+++ polish-notation/calculator/calculate-rpn.go
@@ -2,6 +2,8 @@ package calculator
 
 import (
 	"bufio"
+	"io"
+	"log"
 	"os"
 	"polish-notation/structure"
 	"regexp"
@@ -11,19 +13,25 @@ import (
 
 func Calculate(filePath string) int64 {
 	stack := structure.Stack{}
-	f, err := os.Open(filePath)
+	file, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
+	defer file.Close()
 
-	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(file)
+	variablesMap := scanVariables(file)
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		log.Fatal(err)
+	}
 	scanner.Scan()
 	query := scanner.Text()
 	tokens := strings.Fields(query)
 	for _, tok := range tokens {
-		charIsNum, _ := isNum(tok)
-		if charIsNum {
+		if val, has := variablesMap[tok]; has {
+			stack.Push(val)
+		} else if charIsNum, _ := isNum(tok); charIsNum {
 			stack.Push(tok)
 		} else if isOperator(tok) {
 			operand2, _ := strconv.ParseInt(stack.Pop(), 10, 16)
@@ -33,6 +41,24 @@ func Calculate(filePath string) int64 {
 	}
 	res, _ := strconv.ParseInt(stack.Peek(), 10, 16)
 	return res
+}
+
+func scanVariables(f *os.File) map[string]string {
+	varmap := make(map[string]string)
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.ContainsRune(line, '=') {
+			parts := strings.Split(line, "=")
+			if len(parts) == 2 {
+				varName := strings.TrimSpace(parts[0])
+				val := strings.TrimSpace(parts[1])
+				varmap[varName] = val
+			}
+		}
+	}
+	return varmap
 }
 
 func makeOperation(operand1, operand2 int16, operator string) int16 {
